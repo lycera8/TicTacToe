c5772da70ffc3e8ba3c73149d85c900eacde3d3d
lycera8 <mraycel@gmail.com>
2021-06-05 12:37:58 +0800

    recovery commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,28 +30,220 @@ namespace TicTacToe
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ngBtn = new System.Windows.Forms.Button();
+            this.extBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Khaki;
+            this.btn1.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn1.ForeColor = System.Drawing.Color.Khaki;
+            this.btn1.Location = new System.Drawing.Point(15, 14);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(115, 110);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Khaki;
+            this.btn2.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn2.ForeColor = System.Drawing.Color.Khaki;
+            this.btn2.Location = new System.Drawing.Point(136, 14);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(115, 110);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Khaki;
+            this.btn3.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn3.ForeColor = System.Drawing.Color.Khaki;
+            this.btn3.Location = new System.Drawing.Point(257, 14);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(115, 110);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Khaki;
+            this.btn4.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn4.ForeColor = System.Drawing.Color.Khaki;
+            this.btn4.Location = new System.Drawing.Point(15, 130);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(115, 110);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Khaki;
+            this.btn5.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn5.ForeColor = System.Drawing.Color.Khaki;
+            this.btn5.Location = new System.Drawing.Point(136, 130);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(115, 110);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Khaki;
+            this.btn6.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn6.ForeColor = System.Drawing.Color.Khaki;
+            this.btn6.Location = new System.Drawing.Point(257, 130);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(115, 110);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Khaki;
+            this.btn7.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn7.ForeColor = System.Drawing.Color.Khaki;
+            this.btn7.Location = new System.Drawing.Point(15, 246);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(115, 110);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Khaki;
+            this.btn8.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn8.ForeColor = System.Drawing.Color.Khaki;
+            this.btn8.Location = new System.Drawing.Point(136, 246);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(115, 110);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.Khaki;
+            this.btn9.Font = new System.Drawing.Font("Forte", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn9.ForeColor = System.Drawing.Color.Khaki;
+            this.btn9.Location = new System.Drawing.Point(257, 246);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(115, 110);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Location = new System.Drawing.Point(26, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 375);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // ngBtn
+            // 
+            this.ngBtn.Location = new System.Drawing.Point(26, 449);
+            this.ngBtn.Name = "ngBtn";
+            this.ngBtn.Size = new System.Drawing.Size(118, 29);
+            this.ngBtn.TabIndex = 12;
+            this.ngBtn.Text = "New Game";
+            this.ngBtn.UseVisualStyleBackColor = true;
+            this.ngBtn.Click += new System.EventHandler(this.ngBtn_Click);
+            // 
+            // extBtn
+            // 
+            this.extBtn.Location = new System.Drawing.Point(299, 449);
+            this.extBtn.Name = "extBtn";
+            this.extBtn.Size = new System.Drawing.Size(118, 26);
+            this.extBtn.TabIndex = 13;
+            this.extBtn.Text = "Quit Game";
+            this.extBtn.UseVisualStyleBackColor = true;
+            this.extBtn.Click += new System.EventHandler(this.extBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
-            this.ClientSize = new System.Drawing.Size(571, 507);
+            this.ClientSize = new System.Drawing.Size(441, 521);
+            this.Controls.Add(this.extBtn);
+            this.Controls.Add(this.ngBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic Tac Toe";
-            this.TransparencyKey = System.Drawing.Color.Khaki;
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ngBtn;
+        private System.Windows.Forms.Button extBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,105 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        bool turn = true;
+        int turn_count = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.DimGray;
+
+            if (turn)
+            {
+                btn.Text = "X";
+            }
+            else
+            {
+                btn.Text = "O";
+            }
+
+            turn = !turn;
+            btn.Enabled = false;
+
+            winner_check();    
+        }
+
+        private void winner_check()
+        {
+            bool winner = false;
+
+                //horizontal
+            if (btn1.Text == btn2.Text && btn2.Text == btn3.Text && !btn1.Enabled)
+            {
+                winner = true;
+            }
+            else if (btn4.Text == btn5.Text && btn5.Text == btn6.Text && !btn4.Enabled)
+            {
+                winner = true;
+            }
+            else if (btn7.Text == btn8.Text && btn8.Text == btn9.Text && !btn7.Enabled)
+            {
+                winner = true;
+            }
+
+            if (winner)
+            {
+                stop_the_btns();
+
+                string the_winner = "";
+                if (turn)
+                {
+                    the_winner = "O";
+                }
+                else
+                {
+                    the_winner = "X";
+                }
+
+                MessageBox.Show("The winner is " + the_winner + "!!", "Congratulations!");
+
+
+            }
+        }
+        private void stop_the_btns()
+        {
+            try
+            {
+                foreach (Control bttn in Controls)
+                {
+                    Button btn = (Button)bttn;
+                    btn.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void ngBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void extBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
